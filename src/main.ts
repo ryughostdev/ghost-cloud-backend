59b6b2b1b120e0636e518871b126558ae13233e3
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-06-04 03:35:29 -0600

    chore: Update CORS origin to http://localhost:4321

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:4321',
     credentials: true,
   });
   app.use(
